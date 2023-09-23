680e27e9e55885d68d1123d5796a6ea2a9fb8e5a
Ibrahim Uddin <k21043852@kcl.ac.uk>
2023-09-23 08:23:55 +0100

    python exercise 2"

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,14 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    string_items = [str(item) for item in items]
+
     frequencies = {}
-    # Your code goes here
+    for item in string_items:
+        if item not in frequencies.keys():
+            frequencies[str(item)] = 1
+        else:
+            frequencies[str(item)] = frequencies[item]+1
     return frequencies
+
+print(frequencies([100, 'Hello', '100', '100', 100]))
